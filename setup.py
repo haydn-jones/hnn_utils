64737418708f12a8d37f3324f39630ac934b60bf
Haydn Jones <haydnjonest@gmail.com>
2023-11-04 17:31:11 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hnn_utils",
-    version="0.1.8",
+    version="0.1.9",
     packages=find_packages(),
     install_requires=[
         "torch",
