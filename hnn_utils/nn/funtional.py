61c7fdf5d528b960413bf569608127ecdee7521e
Haydn Jones <haydnjonest@gmail.com>
2023-12-01 15:13:43 -0500

    fml

--- hnn_utils/nn/funtional.py
+++ hnn_utils/nn/funtional.py
@@ -3,30 +3,31 @@ from torch import Tensor
 import math
 
 
-def upbounded_omega(p: Tensor, omega: float = 1.0):
+def upbounded_omega(p: Tensor, min_sigma: float = 0.01, max_sigma: float = 1.0):
     """
     More stable parameterization of variance, in addition the variance is capped at omega (default 1).
     See https://arxiv.org/abs/2106.13739 eqn. 12 for details
     """
     mask = p <= 0
 
-    ppos = p.clamp(0.0).mul(-1.0).exp()
+    ppos = p.clamp_min(0.0).mul(-1.0).exp()
     pneg = p.clamp_max(0.0).exp()
 
     sigma = torch.where(
         mask,
-        (omega / 2) * pneg,
-        (omega / 2) * (2.0 - ppos),
+        (max_sigma / 2) * pneg,
+        (max_sigma / 2) * (2.0 - ppos),
     )
 
-    eps = 1e-5
-    lo = math.log(omega / 2.0)
+    lo = math.log(max_sigma / 2.0)
     logsigma = torch.where(
         mask,
         p + lo,
-        (2.0 - ppos).clamp_min(eps).log() + lo,
+        (2.0 - ppos).log() + lo,
     )
-    return sigma.type_as(p), logsigma.type_as(p)
+    return sigma.clamp_min(min_sigma).type_as(p), logsigma.clamp_min(
+        math.log(min_sigma)
+    ).type_as(p)
 
 
 def gaussian_kl(
